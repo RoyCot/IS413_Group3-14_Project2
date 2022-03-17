933b8a53b6ea3f5683f5769cc7eadf9433b1d0c2
Royce Cotcher <RoyCot@users.noreply.github.com>
2022-03-17 14:33:11 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Group3_14_Project2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
