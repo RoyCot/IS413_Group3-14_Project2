5ca030d7f535ae867f2b118c3c73ad2b1f0b8590
Saberrytwo <90337853+Saberrytwo@users.noreply.github.com>
2022-03-21 10:58:50 -0600

    Context file and initial models/migrations for tourtimes

---
+++ Models/TourTimes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Group3_14_Project2.Models
+{
+    public class TourTimes
+    {
+        [Key]
+        [Req]
+        public int TimeId { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
---
+++ Models/TourTimesContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Group3_14_Project2.Models
+{
+    public class TourTimesContext : DbContext
+    {
+        public TourTimesContext(DbContextOptions<TourTimesContext> options) : base(options)
+        {
+            //Leave blank for now
+        }
+        public DbSet<TourTimes> Times { get; set; }
+        protected override void OnModelCreating(ModelBuilder mb)  //as long as its in the chain/inherited its ok
+        {
+            mb.Entity<TourTimes>().HasData( //This seeds the database with some initial entries
+                new TourTimes
+                {
+                    TimeId = 1,
+                    Time = new DateTime(2022, 3, 23, 8, 0, 0)
+
+
+                },
+                new TourTimes
+                {
+                    TimeId = 2,
+                    Time = new DateTime(2022, 3, 23, 9, 0, 0)
+                }
+
+                );
+        }
+    }
+}
