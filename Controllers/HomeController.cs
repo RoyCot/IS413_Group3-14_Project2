6c29ed47870e309d0601addfa60933ffcc5aecb5
Royce Cotcher <RoyCot@users.noreply.github.com>
2022-03-23 15:01:15 -0600

    Database/Migration fix w/ other minor changes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -37,74 +37,114 @@ namespace Group3_14_Project2.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult AppointmentForm(Appointments appointment)
+        public IActionResult AppointmentForm(Appointments appointment, string time, string date)
         {
-            var entry = context.Times.Single(x => x.Date == appointment.Date);
-            var avail = appointment.Time.Remove(appointment.Time.Length - 3);
-            var availability = ("Available" + avail);
-            if(availability == "Available1")
+            if (ModelState.IsValid)
             {
-                entry.Available1 = false;
-            }
-            else if (availability == "Available2")
-            {
-                entry.Available2 = false;
-            }
-            else if (availability == "Available3")
-            {
-                entry.Available3 = false;
-            }
-            else if (availability == "Available4")
-            {
-                entry.Available4 = false;
-            }
-            else if (availability == "Available5")
-            {
-                entry.Available5 = false;
-            }
-            else if (availability == "Available6")
-            {
-                entry.Available6 = false;
-            }
-            else if (availability == "Available7")
-            {
-                entry.Available7 = false;
-            }
-            else if (availability == "Available8")
-            {
-                entry.Available8 = false;
-            }
-            else if (availability == "Available9")
-            {
-                entry.Available9 = false;
-            }
-            else if (availability == "Available10")
-            {
-                entry.Available10 = false;
-            }
-            else if (availability == "Available11")
-            {
-                entry.Available11 = false;
-            }
-            else if (availability == "Available12")
-            {
-                entry.Available12 = false;
+                var entry = context.Times.Single(x => x.Date == appointment.Date);
+                var avail = appointment.Time.Remove(appointment.Time.Length - 3);
+                var availability = ("Available" + avail);
+                if (availability == "Available1")
+                {
+                    entry.Available1 = false;
+                }
+                else if (availability == "Available2")
+                {
+                    entry.Available2 = false;
+                }
+                else if (availability == "Available3")
+                {
+                    entry.Available3 = false;
+                }
+                else if (availability == "Available4")
+                {
+                    entry.Available4 = false;
+                }
+                else if (availability == "Available5")
+                {
+                    entry.Available5 = false;
+                }
+                else if (availability == "Available6")
+                {
+                    entry.Available6 = false;
+                }
+                else if (availability == "Available7")
+                {
+                    entry.Available7 = false;
+                }
+                else if (availability == "Available8")
+                {
+                    entry.Available8 = false;
+                }
+                else if (availability == "Available9")
+                {
+                    entry.Available9 = false;
+                }
+                else if (availability == "Available10")
+                {
+                    entry.Available10 = false;
+                }
+                else if (availability == "Available11")
+                {
+                    entry.Available11 = false;
+                }
+                else if (availability == "Available12")
+                {
+                    entry.Available12 = false;
+                }
+                else
+                {
+                    entry.Available13 = false;
+                }
+                context.Times.Update(entry);
+                context.Add(appointment);
+                context.SaveChanges();
+
+                return View("Index");
             }
             else
             {
-                entry.Available13 = false;
+                ViewBag.Time = time;
+                ViewBag.Date = date;
+                return View();
             }
-            context.Times.Update(entry);
-            context.Add(appointment);
-            context.SaveChanges();
-
-            return View("Index");
         }
 
         public IActionResult Appointments()
         {
             var Appointments = context.Appointments.ToList();
             return View(Appointments);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int appointmentId)
+        {
+            ViewBag.GroupName = context.Appointments.ToList();
+            var appointment = context.Appointments.Single(x => x.TourId == appointmentId);
+            return View("AppointmentForm", appointment);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Appointments ap)
+        {
+            context.Update(ap);
+            context.SaveChanges();
+            return RedirectToAction("Appointments");
+        }
+
+        [HttpGet]
+        public IActionResult Cancel(int appointmentId)
+        {
+            var appointment = context.Appointments.Single(x => x.TourId == appointmentId);
+            return View(appointment);
+        }
+
+        [HttpPost]
+        public IActionResult Cancel(Appointments ap)
+        {
+            context.Appointments.Remove(ap);
+            context.SaveChanges();
+            return RedirectToAction("Appointments");
         }
     }
 }
--- Models/Appointments.cs
+++ Models/Appointments.cs
@@ -11,11 +11,16 @@ namespace Group3_14_Project2.Models
         [Key]
         [Required]
         public int TourId { get; set; }
+        [Required]
         public string Date { get; set; }
+        [Required]
         public string Time { get; set; }
+        [Required]
         public string GroupName { get; set; }
-        [Range(0, 15, ErrorMessage = "The value, {0}, must be smaller than {1}.")]
+        [Required]
+        [Range(0, 15, ErrorMessage = "The value, {0}, must be between {1} and {2}.")]
         public int GroupSize { get; set; }
+        [Required]
         public string Email { get; set; }
         public string? PhoneNum { get; set; }
     }
